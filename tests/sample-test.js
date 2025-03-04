bc1848d6a61b079783bf25402600b5a349992ebe
nayeemsqa <mdnayeem.cse@gmail.com>
2025-03-04 10:44:48 +0600

    Added Github actions for k6 testing

--- tests/sample-test.js
+++ tests/sample-test.js
@@ -1,17 +1,17 @@
-import http from "k6/http";
-import { check, sleep } from "k6";
+import http from 'k6/http';
+import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 10, // Virtual Users
-  duration: "10s", // Test duration
+  vus: 10, // Simulate 10 users
+  duration: '10s', // Run for 10 seconds
 };
 
 export default function () {
-  let res = http.get("https://test-api.k6.io/public/crocodiles/");
+  let res = http.get('https://jsonplaceholder.typicode.com/posts/1');
 
   check(res, {
-    "is status 200": (r) => r.status === 200,
-    "response time < 500ms": (r) => r.timings.duration < 500,
+    'is status 200': (r) => r.status === 200,
+    'is response time < 500ms': (r) => r.timings.duration < 500,
   });
 
   sleep(1);
